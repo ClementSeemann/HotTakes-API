0ffc9bbac397cad32d6a30369211084e96cc6052
Clement Seemann <clement.seemann44@gmail.com>
2022-08-12 13:02:38 +0200

    Ajout schéma données sauce et utilisateur

--- backend/models/thing.js
+++ backend/models/thing.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
-const thingSchema = mongoose.Schema([
-  {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    imageUrl: { type: String, required: true},
+const thingSchema = mongoose.Schema({
     userId: {type: String, required: true},
-    price: { type: Number, required: true },
-  }
-]);
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true},
+    mainPepper: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    heat: { type: Number, required: true },
+    likes: {type: Number, required: false, default:0},
+    dislikes: {type: Number, required: false, default:0},
+    usersLiked: {type: [String], required: false},
+    usersDisliked: {type: [String], required: false},
+  });
 
 module.exports = mongoose.model('Thing', thingSchema);
